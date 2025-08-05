b4e8009420f7520488b64c0d4a6e3bac14fb79bf
Conroy Makhubu <cmakhubu@gmail.com>
2025-08-05 11:56:03 +0200

    Update my_list.py
    populating my_list.py

--- my_list.py
+++ my_list.py
@@ -1,3 +1,10 @@
 
-#
+#empty_list.py# This file initializes an empty list named `my_list`.
 my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+print(my_list)  
