37fdf18b6ff0089e492739119e4a37da0635f279
Conroy Makhubu <cmakhubu@gmail.com>
2025-08-06 07:56:41 +0200

    Update my_list.py

--- my_list.py
+++ my_list.py
@@ -1,10 +1,13 @@
 
 #empty_list.py# This file initializes an empty list named `my_list`.
 my_list = []
-
+#my_list.py# This file appends several integers to the list `my_list`.
 my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 my_list.append(40)
 
-print(my_list)  
+print(my_list)  # Output the contents of my_list
+# This will print: [10, 20, 30, 40]
+my_list.insert(2, 15)  # Insert 15 at index 2
+print(my_list)  # Output the updated contents of my_list
